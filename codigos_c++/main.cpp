61392554c3c7fe0c8fc5ed30d5a83b348a4bd742
superdariobros22 <darionintendo2@gmail.com>
2024-06-16 17:59:26 +0000

    tablet

--- codigos_c++/main.cpp
+++ codigos_c++/main.cpp
@@ -2,5 +2,5 @@
 using namespace std;
 
 int main() {
-    cout << "24445";
+    cout << "333";
 }
--- codigos_c++/pruevavav.cpp
+++ codigos_c++/pruevavav.cpp
@@ -2,9 +2,7 @@
 using namespace std;
  
 int main() {
-  vector<int> vec = {1, 2, 3, 4, 5};
-  vec.erase(vec.begin()+2);
-  for (int i=0 ; i<vec.size() ; i++) {
-    cout << vec[i] << ' ';
-  }
+  int a;
+  cin >> a;
+  cout << a;
 }
