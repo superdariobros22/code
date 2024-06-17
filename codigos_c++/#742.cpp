49415e5852fd2044dd176e02564132ac1c476063
superdariobros22 <darionintendo2@gmail.com>
2024-06-17 14:15:26 +0000

    table

---
+++ codigos_c++/#742.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ( ) {
+    int a;
+    cin >> a;
+    vector<char> vec;
+
+    for (int i=0 ; i<3 ; i++) {
+        vector<int> 
+    }
+
+
+}
